b77135b51881592f2ba64b9e129ee25026fcfc1d
Sudhanshu Bisht <bishtsudhanshu99@gmail.com>
2026-01-15 01:45:14 +0530

    exploring validator

--- src/app.js
+++ src/app.js
@@ -21,6 +21,73 @@ connectDB().then(()=>{
     console.log("database not connected");
 }); 
 
+// Without async/await, the MongoDB query returns a Promise, and JavaScript does not wait for it to resolve, so the actual data is not available when we try to send the response.
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body.emailId;
+    console.log(userEmail);
+
+    try{
+        const userData = await User.find({ emailId: userEmail});
+        // const user = await User.find(); oo we can use to get all the data
+        if(userData.length === 0){
+            res.status(400).send("User not found");
+        }else{
+         res.send(userData);   
+        }
+    }catch(e){
+        res.status(400).send("something went wrong");
+    }
+})
+
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.id;
+    console.log(userId); 
+
+    const userData = await User.findOneAndDelete(userId);
+    res.send("user deleted successfully");
+
+})
+
+app.patch("/user/:userId",async(req,res)=>{
+    const userId = req.params?.userId;
+    const data = req.body;
+
+    try{
+        const ALLOWED_FIELDS =  ["gender", "age", "firstName", "lastName", "skills", "password"];
+        const IS_ALLOWED = Object.keys(data).every((k)=>
+            ALLOWED_FIELDS.includes(k)
+        );
+
+        if(!IS_ALLOWED){
+            throw new Error("update not allowed");
+        }
+        if(data?.skills.length > 10){
+            throw new Error("skills cannot be more then 10")
+        }
+        const userData = await User.findByIdAndUpdate({_id :userId},data,{
+        returnDocument: "after",
+        runValidators: true,
+    })
+        res.send("updated successfully");
+    }catch(err){
+        res.status(400).send("update failed " + err.message);
+    }
+    
+})
+
+
+app.get("/users", (req, res) => {
+    const userEmail = req.body.emailId;
+    User.find({ emailId: userEmail })
+      .then(user => {
+          res.send(user);
+      })
+      .catch(err => {
+          res.status(400).send("something went wrong");
+      });
+});
+
+
 app.post("/signup",async(req,res)=>{
     // console.log(req.body);
     const user = new User(req.body)
@@ -36,7 +103,7 @@ app.post("/signup",async(req,res)=>{
         await user.save();
     res.send("user added succesfully");
     }catch(err){
-        res.status(400).send(err.meaasge);
+        res.status(400).send(err.message);
     }
     
 })
--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,31 @@
 const mongoose  = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type:String
+        type:String,
+        required: true,
+        minLength :5,
+        maxLength :50,
     },
     lastName: {
-        type:String
+        type:String,
+        required: true
     },
     emailId: {
-        type:String
+        type:String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("invalid email:" +  value);
+            }
+        }
     },
     password: {
-        type:String
+        type:String,
+        required: true
     },
     age: {
         type: String
@@ -19,6 +33,17 @@ const userSchema = mongoose.Schema({
     gender: {
         type: String
     },
-});
+    about: {
+        type: String,
+        default: "this is default"
+    },
+    skills: {
+        type:[String],
+    },
+},
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model("User", userSchema);
